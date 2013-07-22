148b6b851a88fd46acc99d4562aca0877e987358
roy09 <nextgen.ancestor@gmailcom>
2013-07-22 18:13:20 +0600

    after finishing hw3

--- main.py
+++ main.py
@@ -1,50 +1,73 @@
-import os
 import webapp2
+import os
 import jinja2
+import re
 
 from google.appengine.ext import db
 
-template_dir=os.path.join(os.path.dirname(__file__),"templates")
-jinja_env=jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),autoescape=True)
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+								autoescape = True)
+								
+def render_str(template, **params):
+	t = jinja_env.get_template(template)
+	return t.render(params)
 
-class Handler(webapp2.RequestHandler):
+class BlogHandler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
-
+		
 	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
-
+		return render_str(template, **params)
+		
+		
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
-class Art(db.Model):
-    title = db.StringProperty(required = True)
-    art = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+class Post(db.Model):
+	subject = db.StringProperty(required = True)
+	content = db.StringProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	last_modified = db.DateTimeProperty(auto_now = True)
+	
+	def render(self):
+		self._render_text = self.content.replace('\n', '<br>')
+		return render_str("post.html", p = self)
 
-
-class MainPage(Handler):
+class MainPage(BlogHandler):
     def get(self):
-    	self.render_front()
-    
-    def render_front(self, title="", art="", error=""):
-        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC ")
-        self.render("data.html", title=title, art=art, error=error, arts=arts)
-        
+		posts = db.GqlQuery("select * from Post order by created desc limit 10")
+		self.render("front.html", posts = posts)
 
-    def post(self):
-    	title = self.request.get("title")
-    	art = self.request.get("art")
-    	
-    	if title and art:
-            a = Art(title = title, art = art)
-            a.put()
-            self.redirect("/")
-    	else:
-    		error = "damn, that's some error"
-    		self.render_front(title, art, error)
+class NewPost(BlogHandler):
+	def get(self):
+		self.render("newpost.html")
+		
+	def post(self):
+		subject = self.request.get('subject')
+		content = self.request.get('content')
+		
+		if subject and content:
+			p = Post(subject = subject, content = content)
+			p.put()
+			self.redirect('/%s' % str(p.key().id()))
+		else:
+			error = "Both subject and content please!"
+			self.render("newpost.html", subject = subject, content = content, error = error)
 
+class Permalink(BlogHandler):
+	def get(self, post_id):
+		key = db.Key.from_path("Post", int(post_id))
+		post = db.get(key)
+		
+		if not post:
+			self.error(404)
+			return
+		
+		self.render("permalink.html", post = post)
+		
 app = webapp2.WSGIApplication([
-	('/', MainPage),
-	], debug = True)
+    ('/', MainPage),
+    ('/newpost', NewPost),
+    ('/([0-9]+)', Permalink),
+], debug=True)
